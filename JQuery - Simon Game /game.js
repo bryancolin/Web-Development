75a487512fbc13f515cc37e4064824bff01a9bff
Bryan Colin <bryancolin35@ymail.com>
2020-12-10 13:01:37 +0700

    Update JQuery

---
+++ JQuery - Simon Game /game.js
@@ -0,0 +1,82 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var start = false;
+
+$(document).keydown(function () {
+  if (!start) {
+    nextSequence();
+    start = true;
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  animatePress(userChosenColour);
+  playSound(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+  $("#level-title").text("Level " + ++level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    // console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    // console.log("wrong");
+    gameOver();
+    startOver();
+  }
+}
+
+function gameOver() {
+  playSound("wrong");
+
+  $("body").addClass("game-over");
+  setTimeout(function () {
+    $("body").removeClass("game-over");
+  }, 200);
+
+  $("#level-title").text("Game Over, Press Any Key to Restart");
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  start = false;
+}
